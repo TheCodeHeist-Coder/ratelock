669114a808b5300dde2690f46b7f402d047b2210
Rajkumar <thecodeheist01@gmail.com>
2026-05-26 19:44:02 +0530

    landing page designed and added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import LoginPage from './screens/LoginPage'
 import RegisterPage from './screens/RegisterPage'
+import LandingPage from './screens/LandingPage'
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <Routes>
         <Route path='/login' element={<LoginPage />} />
         <Route path='/register' element={<RegisterPage />} />
-        <Route path='/' element={<RegisterPage />} />
+        <Route path='/' element={<LandingPage />} />
       </Routes>
     </BrowserRouter>
   )
